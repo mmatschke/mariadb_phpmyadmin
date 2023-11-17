051c9fb973dd04cd0d2deebef66d363dc6a73460
Manfred Matschke <manfred.matschke@tantumsana.de>
2023-11-17 14:30:03 +0100

    changes on the connection to the database and update the readme.md

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # mariadb_phpmyadmin
 docker-compose yaml with phpMyAdmin, configuration and init script for use the database
+
+## Initialize
+### Change Root Password
+Change the MYSQL_ROOT_PASSWORD in docker-compose.yml and init.sql (for create a root user with connection for any host)
+
+### Create and start containers
+``` shell
+docker-compose up -d
+```
+
+## Connection with phpMyAdmin
+URL: [phpMyAdmin](http://0.0.0.0:8081/)
+
+User: root
+
+Password: [see yaml-file]
+
--- init.sql
+++ init.sql
@@ -1,6 +1,9 @@
-CREATE USER 'root'@'%' IDENTIFIED BY '!msd4SQLAdmin%'; 
+-- for set password use the following create user statement and delete the empty one
+-- CREATE USER 'root'@'%' IDENTIFIED BY 'password';
+CREATE USER 'root'@'%' IDENTIFIED BY '';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
 CREATE DATABASE IF NOT EXISTS dev;
 CREATE DATABASE IF NOT EXISTS test;
+-- example for use database and create table and
 -- USE dev;
 -- CREATE TABLE IF NOT EXISTS (...);
